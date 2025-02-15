d1064f2f3dec6dd10d5618664419720a480ec383
HARISH SS <harishssdev@gmail.com>
2025-02-15 22:24:47 +0530

    mysql setup

--- backend/socialCommunity/urls.py
+++ backend/socialCommunity/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for community project.
+URL configuration for socialCommunity project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
