463495677826fe15cba19025a11617167638bf3d
sudhirr@karunya.edu.in <sudhirr@karunya.edu.in>
2025-02-16 12:41:28 +0530

    login & signup backend setup

--- backend/communityApp/views.py
+++ backend/communityApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer
+from .serializers import UserRegistrationSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(request, email=email, password=password)
+
+            if user:
+                tokens = get_tokens_for_user(user)
+                return Response({
+                    "message": "Login successful",
+                    "tokens": tokens
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
