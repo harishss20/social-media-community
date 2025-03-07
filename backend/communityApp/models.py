b17702a96a843667d04d68ef6fbea7391b788a8f
HARISH SS <harishssdev@gmail.com>
2025-03-07 11:25:01 +0530

    search for community is updated

--- backend/communityApp/models.py
+++ backend/communityApp/models.py
@@ -46,8 +46,8 @@ class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True, null=True)
-    profileImage_url = models.URLField(blank=True, null=True)
-    bannerImage_url = models.URLField(blank=True, null=True)
+    profileImage_url = models.URLField(default="https://res.cloudinary.com/dttdxreiq/image/upload/v1740721608/x4nd59qhqts2l670xzwx.png")
+    bannerImage_url = models.URLField(default="https://res.cloudinary.com/dttdxreiq/image/upload/v1740691299/vlcgkfx6ul17fvokugpv.png")
     date_joined = models.DateField(auto_now_add=True, null=True)
     user_status = models.BooleanField(default=False)
     
@@ -61,8 +61,8 @@ class Community(models.Model):
     community_based_on = models.CharField(max_length=50)
     rules = models.TextField(max_length=500, blank=True, null=True)
     members = models.ManyToManyField(Profile, related_name="communities_joined", blank=True)  
-    communityImage_url = models.URLField(blank=True, null=True)
-    bannerImage_url = models.URLField(blank=True, null=True)
+    communityImage_url = models.URLField(default="https://res.cloudinary.com/dttdxreiq/image/upload/v1740721608/x4nd59qhqts2l670xzwx.png")
+    bannerImage_url = models.URLField(default="https://res.cloudinary.com/dttdxreiq/image/upload/v1740691299/vlcgkfx6ul17fvokugpv.png")
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="communities_created")
     created_at = models.DateTimeField(auto_now_add=True)
 
