54c2b75aef4841b5ea99188a7282abe6bcde5f87
HARISH SS <harishssdev@gmail.com>
2025-03-16 22:20:55 +0530

    Join community is done...

--- frontend/app/join-community/page.jsx
+++ frontend/app/join-community/page.jsx
@@ -49,17 +49,35 @@ export default function JoinCommunity() {
       created_at: "2025-02-27T09:20:15.499940Z",
     },
   ];
+
+  const [communityName, setCommunityName] = useState([]);
+  const handleFromChild = (data) => {
+    if (communityName.includes(data)) {
+      let removedArray = communityName.filter((item) => item !== data);
+      setCommunityName(removedArray);
+    } else {
+      setCommunityName([...communityName, data]);
+    }
+  };
+
   return (
     <div className="flex flex-grow justify-center items-center h-screen bg-[#1a1a1a]">
       <div className="w-[90%] max-w-lg bg-[#343538] p-6 rounded-xl shadow-md">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-white text-xl font-semibold">Join Community</h1>
-          <button className="py-2 px-4 duration-300">continue</button>
+          <button className="py-2 px-4 duration-300">
+            {communityName.length != 0 ? "continue" : "skip"}
+          </button>
         </div>
 
         <div className=" mt-12 space-y-6">
           {data.map((communities) => (
-            <CommunityCard key={communities.id} communities={communities} />
+            <CommunityCard
+              key={communities.id}
+              communities={communities}
+              handleFromChild={handleFromChild}
+              communityName={communityName}
+            />
           ))}
         </div>
       </div>
@@ -67,7 +85,19 @@ export default function JoinCommunity() {
   );
 }
 
-function CommunityCard({ communities }) {
+function CommunityCard({ communities, handleFromChild }) {
+  const [join, setJoin] = useState(false);
+
+  const handleJoinButton = (data) => {
+    const status = !join;
+    setJoin(status);
+    if (status) {
+      handleFromChild(data);
+    } else {
+      handleFromChild(data);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between  bg-[#2b2c2e] px-4 py-3 rounded-lg">
       <div className="flex items-center space-x-4">
@@ -76,10 +106,15 @@ function CommunityCard({ communities }) {
           alt="Community"
           className="h-16 rounded-full"
         />
-        <p className="text-white text-lg ">{communities.name}</p>
+        <p className="text-white text-lg truncate">{communities.name}</p>
       </div>
-      <button className="px-4 py-2 bg-black text-white rounded-lg duration-300">
-        Join
+      <button
+        className="px-4 py-2 bg-black text-white rounded-lg duration-300"
+        onClick={() => {
+          handleJoinButton(communities.name);
+        }}
+      >
+        {join ? "Joined" : "Join"}
       </button>
     </div>
   );
